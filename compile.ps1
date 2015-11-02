5d74a13077371c6325d0eac2dcb6585cb8c500d1
Stefan Eng <eng_steff@hotmail.com>
2015-11-02 10:16:28 +0100

    Catch and disregard exception when traversing files.

--- compile.ps1
+++ compile.ps1
@@ -3,7 +3,7 @@ $file_type = "*.c"
 
 $executables = "executables"
 $win_executables = "$executables\win"
-$flags = "-static -lglfw3 -Iglfw\include -Iglfw\deps -Iglfw\src -lgdi32 -lopengl32 -Wl,-subsystem,windows -I. libportaudio.dll.a libportaudio-2.dll -lpthread -std=c99 -pedantic -Wall -Wextra"
+$flags = "-lglfw3 -Iglfw\include -Iglfw\deps -Iglfw\src -lgdi32 -lopengl32 -Wl,-subsystem,windows -I. libportaudio.dll.a libportaudio-2.dll -lpthread -std=c99 -pedantic -Wall -Wextra"
 
 $flag_list = $flags.split()
 
@@ -15,16 +15,19 @@ if(!(Test-Path -Path $win_executables)) {
   New-Item -ItemType directory -Path $win_executables
 }
 
-
-
-Get-ChildItem -Path $src | ForEach-Object {
-  if ($_.Name -like $file_type) { 
-    Write-Output $_.Name
-    $stub_name = $_.Name.substring(0,$_.Name.length-2)
-    $executable_name = "$stub_name.exe"
-    Write-Output $executable_name
-    $output_file = "$win_executables\$executable_name" 
-    Write-Output $output_file
-    mingw32-gcc $_.Name -o $output_file $flag_list
+Try {
+  Get-ChildItem -Path $src | ForEach-Object {
+    if ($_.Name -like $file_type) { 
+      Write-Output $_.Name
+      $stub_name = $_.Name.substring(0,$_.Name.length-2)
+      $executable_name = "$stub_name.exe"
+      Write-Output $executable_name
+      $output_file = "$win_executables\$executable_name" 
+      Write-Output $output_file
+      mingw32-gcc $_.Name -o $output_file $flag_list
+    }
   }
+}
+Catch [System.NotSupportedException] {
+  Write-Host "exception.."
 }
