b7ab1f3d434c8c4a561f9bf0cecbe64925aadb1a
Stefan Eng <eng_steff@hotmail.com>
2015-11-02 09:53:26 +0100

    Add linux subfolder in executables.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #bin/sh
 flags="-lGLEW -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -lXxf86vm -lm -ldl -lXinerama -lXcursor -lrt -lasound -lpthread -lportaudio"
 executable_dir="executables"
+linux_executables="linux"
 #compiler="g++"
 compiler="cc"
 file_ending=".c"
@@ -9,12 +10,17 @@ if [ ! -d "$executable_dir" ]; then
   mkdir "$executable_dir"
 fi
 
+if [ ! -d "$executable_dir/$linux_executables" ]; then
+  mkdir "$executable_dir/$linux_executables"
+fi
+
 if [ ! -z "$1" ]; then
   case "$1" in
     "--clean")
       echo "Cleaning out executables and recompiling.."
       rm -rf "$executable_dir"
       mkdir "$executable_dir"
+      mkdir "$executable_dir/$linux_executables"
     ;;
     *)
       echo `printf "No such command: %s" $1`
@@ -27,7 +33,7 @@ for file in ./*; do
   case "$file" in
     *"$file_ending"*)
       exec_name=$(echo $file | cut -c 2- | rev | cut -c 3- | rev)
-      $compiler "$file" -o "${executable_dir}/${exec_name}.exe" $flags
+      $compiler "$file" -o "${executable_dir}/$linux_executables/${exec_name}.exe" $flags
       ;;
   esac
 done
