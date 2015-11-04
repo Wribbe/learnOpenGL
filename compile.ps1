28c0d2bb3b31d59068ec9a4e53a2762a2f459224
Stefan Eng <eng_steff@hotmail.com>
2015-11-04 10:43:51 +0100

    Include error flags and glew includes.

--- compile.ps1
+++ compile.ps1
@@ -2,11 +2,13 @@ $src = ".\source"
 $file_type = "*.c"
 $include_dir = "include_compilation"
 $glfw_include = "$include_dir\glfw"
+$glew_include = "$include_dir\glew"
+$gamedev_folder = "Y:\gamedev"
 
 $executables = "executables"
 $win_executables = "$executables\win"
 
-$flags = "-lglfw3 -I$glfw_include\include -I$glfw_include\deps -I$glfw_include\src -lgdi32 -lopengl32 -Wl,-subsystem,windows -I. $include_dir\libportaudio.dll.a -lpthread -std=c99 -pedantic -Wall -Wextra -I$include_dir"
+$flags = "-lglfw3 -I$glfw_include\include -I$glfw_include\deps -I$glfw_include\src -lgdi32 -lopengl32 -Wl,-subsystem,windows -I. $include_dir\libportaudio.dll.a $include_dir\libglew32.a $include_dir\libglew32.dll.a -lpthread -std=c99 -pedantic -Wall -Wextra -I$include_dir" 
 
 $flag_list = $flags.split()
 
@@ -34,3 +36,4 @@ Try {
 Catch [System.NotSupportedException] {
   Write-Host "exception.."
 }
+cp $win_executables\* $gamedev_folder
