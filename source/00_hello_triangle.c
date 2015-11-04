4097d7c7d7c24ecaf244d9766bdf4f4e2b76b825
Stefan Eng <eng_steff@hotmail.com>
2015-11-04 10:45:58 +0100

    Triangle compiles and runs on windows and linux.

--- source/00_hello_triangle.c
+++ source/00_hello_triangle.c
@@ -1,12 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 GLfloat vertices[] = {
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
-     0.0f, -0.5f, 0.0f
+     0.0f,  0.5f, 0.0f
 };
+
+GLuint VBO;
+
+const GLchar* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 position;\n"
+    "\n"
+    "void main() {\n"
+    "   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "}\n";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main() {\n"
+    "   color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n";
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    (void) scancode;
+    (void) mode;
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
 
 int main(void) {
     GLFWwindow* window;
@@ -15,7 +39,13 @@ int main(void) {
         return -1;
     }
 
-    window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    //window = glfwCreateWindow(800, 600, "Hello Triangle", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Hello Triangle WIN", NULL, NULL);
     if(!window) {
         glfwTerminate();
         return -1;
@@ -23,14 +53,86 @@ int main(void) {
 
     glfwMakeContextCurrent(window);
 
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        printf("Failed to initialize GLEW\n");
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetKeyCallback(window, key_callback);
+
+    GLint success;
+    GLchar infoLog[512];
+
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        fprintf(stderr,"Error: Vertex shader failed with:\n  %s",infoLog);
+    }
+
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        fprintf(stderr,"Error: Fragment shader failed with:\n %s",infoLog);
+    }
+
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        fprintf(stderr,"Error: Shader linkage failed with:\n  %s",infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+
     while(!glfwWindowShouldClose(window)) {
+        /* Poll and process events */
+        glfwPollEvents();
+
         /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll and process events */
-        glfwPollEvents();
     }
 
     glfwTerminate();
