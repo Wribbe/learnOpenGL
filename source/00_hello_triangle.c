100ba6ec30f119ee99abdda0c189cee8709e83b9
Stefan Eng <eng_steff@hotmail.com>
2015-11-04 11:00:31 +0100

    Drawing two triangles (rectangle) using Element Buffer Object (EBO).

--- source/00_hello_triangle.c
+++ source/00_hello_triangle.c
@@ -3,13 +3,22 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-};
+//GLfloat vertices[] = {
+//    -0.5f, -0.5f, 0.0f,
+//     0.5f, -0.5f, 0.0f,
+//     0.0f,  0.5f, 0.0f
+//};
 
-GLuint VBO;
+GLfloat vertices[] = {
+    0.5f,  0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+   -0.5f, -0.5f, 0.0f,
+   -0.5f,  0.5f, 0.0f
+};
+GLuint indices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
 
 const GLchar* vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 position;\n"
@@ -107,15 +116,20 @@ int main(void) {
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
+    GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
-
 
     while(!glfwWindowShouldClose(window)) {
         /* Poll and process events */
@@ -127,7 +141,8 @@ int main(void) {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         /* Swap front and back buffers */
