c7f90329da5b08fd198213df2059ab94128dfbdc
Stefan Eng <eng_steff@hotmail.com>
2015-11-05 17:32:08 +0100

    Use file_utils in 01_shaders.c

--- source/01_shaders.c
+++ source/01_shaders.c
@@ -3,6 +3,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "file_utils.h"
+
 #define UNUSED(x) (void)x
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
