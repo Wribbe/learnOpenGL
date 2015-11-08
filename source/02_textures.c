c66ad653df33c86730930c58405e03f699cb63cb
Stefan Eng <eng_steff@hotmail.com>
2015-11-08 20:07:15 +0100

    Initialize GLFW context and create window.

---
+++ source/02_textures.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "file_utils.h"
+
+int main(void) {
+
+    GLFWwindow *window;
+
+    if(!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW, aborting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    GLint WIDTH, HEIGHT;
+    WIDTH = 800;
+    HEIGHT = 600;
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "02 textures", NULL, NULL);
+    if(!window) {
+        fprintf(stderr, "Failed to create window, aborting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+
+
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
