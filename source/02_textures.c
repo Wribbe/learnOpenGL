468a60b48705c9d093617117de12a91ef869f558
Stefan Eng <eng_steff@hotmail.com>
2015-11-09 10:48:08 +0100

    Sample and render texture, don't forget to use the shader program.

--- source/02_textures.c
+++ source/02_textures.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <SOIL.h>
 
 #include "file_utils.h"
 
@@ -16,9 +17,22 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 GLfloat vertices[] = {
-    0.0f,  0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-   -0.5f, -0.5f, 0.0f,
+    // Position         // Colors           // Texture Coords
+    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+};
+
+GLfloat tex_coords[] = {
+    0.0f, 0.0f,
+    1.0f, 0.0f,
+    0.5f, 1.0f,
+};
+
+GLuint indices[] = {
+    0, 1, 3,
+    1, 2, 3,
 };
 
 int main(void) {
@@ -47,12 +61,95 @@ int main(void) {
         return(EXIT_FAILURE);
     }
 
+    char* vertex_source = read_shader("shaders/02.vert");
+    char* fragment_source = read_shader("shaders/02.frag");
+
+    GLuint vertex_shader, fragment_shader, shader_program;
+
+    vertex_shader = compile_shader(vertex_source, GL_VERTEX_SHADER);
+    fragment_shader = compile_shader(fragment_source, GL_FRAGMENT_SHADER);
+
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, info_log_size, &success);
+    if(!success) {
+        glGetProgramInfoLog(shader_program, info_log_size, NULL, info_log);
+        fprintf(stderr, "Error: Shader linkage failed with:\n %s",info_log);
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    free(vertex_source);
+    free(fragment_source);
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                     GL_STATIC_DRAW);
+
+    GLsizei stride = 8 * sizeof(GLfloat);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride,
+                          (GLvoid*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride,
+                          (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride,
+                          (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    int width, height;
+    unsigned char* image = SOIL_load_image("textures/02_container.jpg",
+                                           &width,
+                                           &height,
+                                           0,
+                                           SOIL_LOAD_RGB);
+    if (!image) {
+        fprintf(stderr, "Could not load image.\n");
+    }
+    fprintf(stderr, "result: %s\n",SOIL_last_result());
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
     glfwSetKeyCallback(window, key_callback);
 
     while(!glfwWindowShouldClose(window)) {
 
         glfwPollEvents();
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
     }
