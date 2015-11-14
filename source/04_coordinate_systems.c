a54c2bc168efbe0cc27ffe1975dea5b62334021b
Stefan Eng <eng_steff@hotmail.com>
2015-11-14 19:32:37 +0100

    Initialize environment and create window.

--- source/04_coordinate_systems.c
+++ source/04_coordinate_systems.c
@@ -3,7 +3,33 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define UNUSED(x) (void)x
+
 int main(void) {
+
+    if (!glfwInit()) {
+        fprintf(stderr, "Could not initialize GLFW, aborting.\n");
+        return(EXIT_FAILURE);
+    }
+
+    GLFWwindow* window;
+
+    int WIDTH, HEIGHT;
+    WIDTH = 800;
+    HEIGHT = 600;
+
+    glfwCreateWindow(WIDTH, HEIGHT, "04 coordinate systems.", NULL, window);
+    if (!window) {
+        fprintf(stderr, "Could not create window, aborting.\n");
+        return(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK) {
+        fprintf(stderr, "Could not initialize GLEW, aborting.\n");
+        return(EXIT_FAILURE);
+    }
+
     printf("Hello.\n");
     return EXIT_SUCCESS;
 }
