03d1908ee36d29ad4737e4c6927109468806e3c6
Stefan Eng <eng_steff@hotmail.com>
2015-11-14 21:49:12 +0100

    Set up and draw a red quad.

--- source/04_coordinate_systems.c
+++ source/04_coordinate_systems.c
@@ -61,11 +61,42 @@ int main(void) {
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride,
+                          (GLvoid*)(3*sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride,
+                          (GLvoid*)(6*sizeof(GLfloat)));
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    char *vertex_source = read_shader("shaders/04.vert");
+    char *fragment_source = read_shader("shaders/04.frag");
+
+    GLuint shader_program;
+    shader_program = create_shader_program(vertex_source, fragment_source);
+
+    glUseProgram(shader_program);
+
     glClearColor(1.0f, 0.5f, 1.0f, 1.0f);
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         glClear(GL_COLOR_BUFFER_BIT);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
     }
